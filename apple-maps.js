6d9780922ccc89bc9bfe2c01f95698436433c002
stevennoad <110924473+stevennoad@users.noreply.github.com>
2024-07-06 19:27:02 +0100

    Version: 1.3.1 refactored for better readability

--- apple-maps.js
+++ apple-maps.js
@@ -1,94 +1,102 @@
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('DOM fully loaded and parsed.');
+		console.log('DOM fully loaded and parsed.');
 
-    mapkit.init({
-        authorizationCallback: function(done) {
-            done(appleMapsSettings.token);
-        }
-    });
+		mapkit.init({
+				authorizationCallback: function(done) {
+						done(appleMapsSettings.token);
+				}
+		});
+		console.log('MapKit initialized.');
 
-    console.log('MapKit initialized.');
+		const CALLOUT_OFFSET = new DOMPoint(0, 15);
+		const CALL_OUT_ANIMATION = ".4s cubic-bezier(0.4, 0, 0, 1.5) 0s 1 normal scale-and-fadein";
 
-    function initializeMap(mapElementId, landmarks) {
-        console.log('Initializing map for element ID: ' + mapElementId);
+		const landmarkAnnotationCallout = {
+				calloutElementForAnnotation: function(annotation) {
+						return createCalloutElement(annotation.landmark);
+				},
+				calloutAnchorOffsetForAnnotation: function() {
+						return CALLOUT_OFFSET;
+				},
+				calloutAppearanceAnimationForAnnotation: function() {
+						return CALL_OUT_ANIMATION;
+				}
+		};
 
-        var CALLOUT_OFFSET = new DOMPoint(0, 15);
-        var landmarkAnnotationCallout = {
-            calloutElementForAnnotation: function(annotation) {
-                return calloutForLandmarkAnnotation(annotation);
-            },
-            calloutAnchorOffsetForAnnotation: function(annotation, element) {
-                return CALLOUT_OFFSET;
-            },
-            calloutAppearanceAnimationForAnnotation: function(annotation) {
-                return ".4s cubic-bezier(0.4, 0, 0, 1.5) 0s 1 normal scale-and-fadein";
-            }
-        };
+		function createCalloutElement(landmark) {
+				const div = document.createElement("div");
+				div.className = "landmark";
 
-        var annotations = landmarks.map(function(landmark) {
-            var annotation = new mapkit.MarkerAnnotation(landmark.coordinate, {
-                callout: landmarkAnnotationCallout,
-                color: "#1d1d1d4",
-                title: landmark.title,
-                glyphText: appleMapsSettings.glyph
-            });
-            annotation.landmark = landmark;
-            return annotation;
-        });
+				const title = div.appendChild(document.createElement("h1"));
+				title.textContent = landmark.title;
 
-        let map = new mapkit.Map(mapElementId, {
-            colorScheme: mapkit.Map.ColorSchemes.Light,
-        });
+				const section = div.appendChild(document.createElement("section"));
 
-        console.log('Map object created for element ID: ' + mapElementId);
+				const address = section.appendChild(document.createElement("p"));
+				address.className = "address";
+				address.textContent = landmark.address;
 
-        map.showItems(annotations);
-        console.log('Annotations added to map for element ID: ' + mapElementId);
-    }
+				const phone = section.appendChild(document.createElement("p"));
+				phone.className = "phone";
+				phone.textContent = landmark.phone;
 
-    function calloutForLandmarkAnnotation(annotation) {
-        var div = document.createElement("div");
-        div.className = "landmark";
+				const hours = section.appendChild(document.createElement("p"));
+				hours.className = "hours";
+				hours.textContent = landmark.hours;
 
-        var title = div.appendChild(document.createElement("h1"));
-        title.textContent = annotation.landmark.title;
+				const link = section.appendChild(document.createElement("p"));
+				link.className = "homepage";
+				const a = link.appendChild(document.createElement("a"));
+				a.href = landmark.url;
+				a.textContent = "Store Details";
 
-        var section = div.appendChild(document.createElement("section"));
-        var address = section.appendChild(document.createElement("p"));
-        address.className = "address";
-        address.textContent = annotation.landmark.address;
+				return div;
+		}
 
-        var phone = section.appendChild(document.createElement("p"));
-        phone.className = "phone";
-        phone.textContent = annotation.landmark.phone;
+		function createAnnotation(landmark) {
+				return new mapkit.MarkerAnnotation(landmark.coordinate, {
+						callout: landmarkAnnotationCallout,
+						color: "#1d1d1d4",
+						title: landmark.title,
+						glyphText: appleMapsSettings.glyph
+				});
+		}
 
-        var hours = section.appendChild(document.createElement("p"));
-        hours.className = "hours";
-        hours.textContent = annotation.landmark.hours;
+		function initializeMap(mapElementId, landmarks) {
+				console.log('Initializing map for element ID: ' + mapElementId);
 
-        var link = section.appendChild(document.createElement("p"));
-        link.className = "homepage";
-        var a = link.appendChild(document.createElement("a"));
-        a.href = annotation.landmark.url;
-        a.textContent = "Store Details";
+				const annotations = landmarks.map(createAnnotation);
 
-        return div;
-    }
+				const map = new mapkit.Map(mapElementId, {
+						colorScheme: mapkit.Map.ColorSchemes.Light,
+				});
+				console.log('Map object created for element ID: ' + mapElementId);
 
-    var mapContainers = document.querySelectorAll('.apple-map-container');
-    console.log('Map containers found:', mapContainers);
+				map.showItems(annotations);
+				console.log('Annotations added to map for element ID: ' + mapElementId);
+		}
 
-    mapContainers.forEach(function(container) {
-        var mapElementId = container.id;
-        var landmarks = [{
-            coordinate: new mapkit.Coordinate(parseFloat(container.getAttribute('data-lat')), parseFloat(container.getAttribute('data-lng'))),
-            title: container.getAttribute('data-title'),
-            address: container.getAttribute('data-address'),
-            phone: container.getAttribute('data-phone'),
-            hours: container.getAttribute('data-hours'),
-            url: container.getAttribute('data-url')
-        }];
-        console.log('Landmarks for ' + mapElementId + ':', landmarks);
-        initializeMap(mapElementId, landmarks);
-    });
+		function getLandmarkFromContainer(container) {
+				return {
+						coordinate: new mapkit.Coordinate(
+								parseFloat(container.getAttribute('data-lat')),
+								parseFloat(container.getAttribute('data-lng'))
+						),
+						title: container.getAttribute('data-title'),
+						address: container.getAttribute('data-address'),
+						phone: container.getAttribute('data-phone'),
+						hours: container.getAttribute('data-hours'),
+						url: container.getAttribute('data-url')
+				};
+		}
+
+		const mapContainers = document.querySelectorAll('.apple-map-container');
+		console.log('Map containers found:', mapContainers);
+
+		mapContainers.forEach(container => {
+				const mapElementId = container.id;
+				const landmarks = [getLandmarkFromContainer(container)];
+				console.log('Landmarks for ' + mapElementId + ':', landmarks);
+				initializeMap(mapElementId, landmarks);
+		});
 });
