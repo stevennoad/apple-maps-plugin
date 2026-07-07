a2115b371129666a5d7d0760d9587a00f6077fdd
stevennoad <110924473+stevennoad@users.noreply.github.com>
2026-07-07 13:25:35 +0100

    chore: version bump

--- apple-maps.php
+++ apple-maps.php
@@ -3,7 +3,7 @@
 Plugin Name: Apple Maps for Elementor
 Plugin URI:  https://github.com/stevennoad/apple-maps-plugin
 Description: A plugin to add Apple Maps with multiple locations as an Elementor widget.
-Version:     3.0.1
+Version:     3.0.2
 Author:      Steve Noad
 License:     MIT
 Text Domain: apple-maps
